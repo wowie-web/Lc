0b6b4ecb79d6d93711170745e69bdacf84665031
wowie-web <arataguray@gmail.com>
2026-05-28 09:23:27 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+// TIME + DATE
+function updateTime() {
+    const now = new Date();
+
+    const time = now.toLocaleTimeString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+
+    const date = now.toLocaleDateString([], {
+        weekday: 'long',
+        month: 'long',
+        day: 'numeric'
+    });
+
+    document.getElementById('time').textContent = time;
+    document.getElementById('date').textContent = date;
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+
+// KEYBOARD
+const keys = "1234567890qwertyuiopasdfghjklzxcvbnm".split("");
+const keyboard = document.getElementById("keyboard");
+const input = document.getElementById("password");
+
+keys.forEach(k => {
+    const btn = document.createElement("div");
+    btn.textContent = k;
+    btn.classList.add("key");
+
+    btn.onclick = () => {
+        input.value += k;
+    };
+
+    keyboard.appendChild(btn);
+});
+
+// SAVE PASSWORD ATTEMPTS
+input.addEventListener("change", () => {
+    let history = JSON.parse(localStorage.getItem("history")) || [];
+    history.push(input.value);
+    localStorage.setItem("history", JSON.stringify(history));
+
+    // ADMIN MODE
+    if (input.value === "admin") {
+        showHistory();
+    }
+
+    input.value = "";
+});
+
+function showHistory() {
+    const historyDiv = document.getElementById("history");
+    const history = JSON.parse(localStorage.getItem("history")) || [];
+
+    historyDiv.classList.remove("hidden");
+    historyDiv.innerHTML = "<b>Password Attempts:</b><br>" + history.join("<br>");
+}
